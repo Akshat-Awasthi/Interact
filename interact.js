32c1ca06014bab3abde334b5176d4717c19d8823
Akshat Awasthi <akshatawasthi@Akshats-MacBook-Air-4.local>
2025-07-23 01:31:32 +0530

    basic interact with babel transpile jsx is implemented

--- interact.js
+++ interact.js
@@ -1,4 +1,4 @@
-function CreateElement(type, props, ...children) {
+function createElement(type, props, ...children) {
     return {
         type,
         props: {
@@ -7,7 +7,7 @@ function CreateElement(type, props, ...children) {
                 if (typeof child === 'object') {
                     return child;
                 } else if (typeof child === 'string' || typeof child === 'number') {
-                    return { type: 'text', props: { nodeValue: String(child) } };
+                    return createTextElement(child);
                 } else {
                     throw new Error('Invalid child type');
                 }
@@ -16,3 +16,38 @@ function CreateElement(type, props, ...children) {
     }
 }
 
+function createTextElement(text){
+    return{
+        type: "TEXT_ELEMENT",
+        props:{
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
+
+function render(element, container){
+    const dom = element.type == "TEXT_ELEMENT" ? document.createTextNode(element.props.nodeValue) : document.createElement(element.type);
+    const isProps = key => key !== "children";
+    Object.keys(element.props).filter(isProps).forEach(name => {dom[name] = element.props[name]})
+    element.props.children.forEach(child => {
+        render(child, dom);
+    });
+    container.appendChild(dom);
+}
+
+const interact = {
+    createElement,
+    render,
+};
+
+/** @jsx interact.createElement */
+const element = (
+    <div>
+    <h1>Hello world from interact</h1>
+    <h2 style="text-align:right"> from Akshat Awasthi</h2>
+    </div>
+)
+
+const container = document.getElementById("root");
+interact.render(element, container);
